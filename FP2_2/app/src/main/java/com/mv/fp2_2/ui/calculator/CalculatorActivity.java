6e48291984afba9591819cc40a2d590d97240cb2
Pedro Miguel De Matos Norberto <2211904@my.ipleiria.pt>
2022-10-10 15:49:03 +0100

    Layout partialy done

---
+++ FP2_2/app/src/main/java/com/mv/fp2_2/ui/calculator/CalculatorActivity.java
@@ -0,0 +1,16 @@
+package com.mv.fp2_2.ui.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mv.fp2_2.R;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Acesso_Movel_a_Sistemas_de_Informacao
-
-Disciplina de do 2º ano, 1º semestre do TeSP
