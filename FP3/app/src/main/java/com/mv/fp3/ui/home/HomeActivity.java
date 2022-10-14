3e45521575bcf00f96ea7b607a4c0524ac106122
Pedro Miguel De Matos Norberto <2211904@my.ipleiria.pt>
2022-10-14 13:50:32 +0100

    login xml feito home xml feito

---
+++ FP3/app/src/main/java/com/mv/fp3/ui/home/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.mv.fp3.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mv.fp3.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+    }
+}
---
+++ FP3/app/src/main/java/com/mv/fp3/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.mv.fp3.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mv.fp3.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
